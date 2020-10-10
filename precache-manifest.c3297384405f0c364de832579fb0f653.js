0fe90ff94a9b47f8fcf6021b7568a34f41607dba
hannad <hanad.rehman@healthifyme.com>
2020-10-10 23:04:56 +0530

    Updates

---
+++ precache-manifest.c3297384405f0c364de832579fb0f653.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386b5956adec5dff31f258fec26cf9f1",
+    "url": "/flow-mgt/index.html"
+  },
+  {
+    "revision": "165fefab2b221c865af1",
+    "url": "/flow-mgt/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "b2c053473720a373b3df",
+    "url": "/flow-mgt/static/css/main.c1d745a0.chunk.css"
+  },
+  {
+    "revision": "165fefab2b221c865af1",
+    "url": "/flow-mgt/static/js/2.08f3cc57.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/flow-mgt/static/js/2.08f3cc57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2c053473720a373b3df",
+    "url": "/flow-mgt/static/js/main.5dde147c.chunk.js"
+  },
+  {
+    "revision": "6b7a1b556410a667a162",
+    "url": "/flow-mgt/static/js/runtime-main.c21822eb.js"
+  }
+]);
