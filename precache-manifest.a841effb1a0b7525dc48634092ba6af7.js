392fdbdb255d9c1cf01073c4ee9b17da7abe1a6e
hannad <hanad.rehman@healthifyme.com>
2020-10-14 00:07:28 +0530

    Updates

--- precache-manifest.a841effb1a0b7525dc48634092ba6af7.js
+++ precache-manifest.a841effb1a0b7525dc48634092ba6af7.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "94b67187aca1cd2a798445d61c221170",
+    "revision": "9d04da8a1d977e10395a11558b427dc6",
     "url": "/flow-mgt/index.html"
   },
   {
-    "revision": "d2d172ae92d98a3d6073",
+    "revision": "ce9556d1b52a3b535abb",
     "url": "/flow-mgt/static/css/2.cd846464.chunk.css"
   },
   {
-    "revision": "9cd0a1b889c9b6a393e1",
+    "revision": "18a15c2484b43715fc4a",
     "url": "/flow-mgt/static/css/main.c1d745a0.chunk.css"
   },
   {
-    "revision": "d2d172ae92d98a3d6073",
-    "url": "/flow-mgt/static/js/2.e8bdc7fd.chunk.js"
+    "revision": "ce9556d1b52a3b535abb",
+    "url": "/flow-mgt/static/js/2.9a8881ff.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/flow-mgt/static/js/2.e8bdc7fd.chunk.js.LICENSE.txt"
+    "url": "/flow-mgt/static/js/2.9a8881ff.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9cd0a1b889c9b6a393e1",
-    "url": "/flow-mgt/static/js/main.c946c0e9.chunk.js"
+    "revision": "18a15c2484b43715fc4a",
+    "url": "/flow-mgt/static/js/main.2751615d.chunk.js"
   },
   {
     "revision": "6b7a1b556410a667a162",
